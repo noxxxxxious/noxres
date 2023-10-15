5d18528820c90994d1ef8074d73239e6ceb220d9
noxxxxxious <sean.fried@gmail.com>
2023-10-15 02:03:31 -0500

    Add rewritten in rust badge

--- src/components/SectionPortfolio.rs
+++ src/components/SectionPortfolio.rs
@@ -3,6 +3,8 @@ use sycamore::prelude::*;
 #[component]
 pub fn SectionPortfolio<G: Html>() -> View<G> {
     view! {
-        section{ "Portfolio section"}
+        section(id="portfolio"){
+            
+        }
     }
 }
--- src/components/SectionTop.rs
+++ src/components/SectionTop.rs
@@ -8,10 +8,14 @@ pub fn SectionTop<G: Html>() -> View<G> {
                 div(class="landing--heading--container"){
                     span(class="landing--heading--name"){ "Sean Fried" }
                     span(class="landing--heading--title"){ "Front End Developer" }
-                    span(class="landing--heading--subtitle"){ "Profile site unnecessarily rewritten in Rust!"}
+                    span(class="landing--heading--subtitle"){ "Vue ┃ Nuxt ┃ MongoDB ┃ Express ┃ Node"}
                 }
                 div(class="landing--picture--container"){
                     div(class="landing--picture--image"){}
+                }
+                div(class="landing--rust"){ 
+                    div{"Rewritten"}
+                    div{"in Rust!"}
                 }
             }
         }
