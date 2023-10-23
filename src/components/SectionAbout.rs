4b4e1f62836400c0b69f1a92f6db44621fbeac36
noxxxxxious <sean.fried@gmail.com>
2023-10-23 01:10:03 -0500

    Finish About section & Profiles section

--- src/components/SectionAbout.rs
+++ src/components/SectionAbout.rs
@@ -3,24 +3,157 @@ use sycamore::prelude::*;
 #[component]
 pub fn SectionAbout<G: Html>() -> View<G> {
     view! {
-        section(id="about"){
-            div(class="header"){ "About" }
-            div(class="about--container"){
-                div(class="about--diamond") {
-                    div(class="about--diamond--background"){}
-                }
-                div(class="about--content-connector") {}
-                div(class="about--info-wrapper") {
-                    div(class="about--info--education") {
-                        div(class="about--info--header") {
-                            "Education"
-                        }
-                        div(class="about--info--content") {
-                            "Antonelli College"
+        section(id="about") {
+            div(class="header") { "About" }
+            div(class="about--container") {
+                div {
+                    div(class="about--content-connector") {}
+                    div(class="about--info-wrapper") {
+                        div(class="about--info--history") {
+                            div(class="about--info--header") {
+                                "History"
+                            }
+                            div(class="about--info--underline")
+                            div(class="about--info--hero") {
+                                "Bramtech Solutions"
+                            }
+                            div(class="about--info--content") {
+                                span { "2021 - Present"}
+                                span { "Central Services & Support Specialist" }
+                                ul{
+                                    li {"PowerShell Automation"}
+                                    li {"Windows Server Administration"}
+                                    li {"Replibit and Shadowcopy Management"}
+                                    li {"Workflow Tool Development using Web Technologies"}
+                                }
+                            }
+                            div(class="about--info--divider-diamond"){
+                                div(class="about--info--divider-diamond--line")
+                            }
+                            div(class="about--info--hero") {
+                                "NOLA Web Hosting"
+                            }
+                            div(class="about--info--content") {
+                                span { "2017 - 2020"}
+                                span { "Front End Developer" }
+                                ul{
+                                    li {"Designing websites for local businesses"}
+                                    li {"HTML & CSS, JavaScript, PHP, etc"}
+                                    li {"Server Management & DNS Configuration"}
+                                    li {"Project planning with clients"}
+                                }
+                            }
                         }
                     }
+                    div(class="about--content-connector bottom")
                 }
-                div(class="about--content-connector bottom") {}
+                div{
+                    div(class="about--diamond") {
+                        div(class="about--diamond--background")
+                    }
+                    div(class="about--content-connector") {}
+                    div(class="about--info-wrapper") {
+                        div(class="about--info--education") {
+                            div(class="about--info--header") {
+                                "Education"
+                            }
+                            div(class="about--info--underline")
+                            div(class="about--info--hero") {
+                                "Antonelli College"
+                            }
+                            div(class="about--info--content") {
+                                span { "Business Technology & Networking"}
+                                span { "Associate in Applied Business" }
+                                span { "2009 - 2011" }
+                            }
+                        }
+                        div(class="about--info--divider-diamond"){
+                            div(class="about--info--divider-diamond--line")
+                        }
+                        div(class="about--info--header") {
+                            "Certifications"
+                        }
+                        div(class="about--info--underline")
+                        div(class="about--info--hero") {
+                            "JLPT N3"
+                        }
+                        div(class="about--info--content") {
+                            span { "2020"}
+                            span { "Credential ID:" }
+                            span { "4021401-30033" }
+                        }
+                        div(class="about--info--divider-diamond"){
+                            div(class="about--info--divider-diamond--line")
+                        }
+                        div(class="about--info--hero") {
+                            "JLPT N5"
+                        }
+                        div(class="about--info--content") {
+                            span { "2016"}
+                            span { "Credential ID:" }
+                            span { "15B4024021401-300331501-50033" }
+                        }
+                    }
+                    div(class="about--content-connector bottom")
+                }
+                div {
+                    div(class="about--content-connector") {}
+                    div(class="about--info-wrapper") {
+                        div(class="about--info--education") {
+                            div(class="about--info--header") {
+                                "Skills"
+                            }
+                            div(class="about--info--underline")
+                            div(class="about--info--hero") {
+                                "Web Technologies"
+                            }
+                            div(class="about--info--content") {
+                                div(class="about--info--skill-list"){
+                                    span {"HTML"}
+                                    span {"CSS"}
+                                    span {"JavaScript"}
+                                    span {"TypeScript"}
+                                    span {"Vue"}
+                                    span {"Nuxt"}
+                                    span {"React"}
+                                    span {"Next"}
+                                    span {"MongoDB"}
+                                    span {"Node"}
+                                    span {"Express"}
+                                    span {"REST"}
+                                    span {"Electron"}
+                                    span {"Vuetify"}
+                                    span {"Bootstrap"}
+                                    span {"jQuery"}
+                                    span {"Rust"}
+                                    span {"Sycamore"}
+                                }
+                            }
+                            div(class="about--info--divider-diamond"){
+                                div(class="about--info--divider-diamond--line")
+                            }
+                            div(class="about--info--hero") {
+                                "Administrative"
+                            }
+                            div(class="about--info--content") {
+                                div(class="about--info--skill-list"){
+                                    span {"Windows"}
+                                    span {"Linux"}
+                                    span {"Windows Server"}
+                                    span {"Active Directory"}
+                                    span {"DNS"}
+                                    span {"LDAP"}
+                                    span {"VSS"}
+                                    span {"Office 365"}
+                                    span {"AzureAD"}
+                                    span {"Exchange Online"}
+                                    span {"PowerShell"}
+                                }
+                            }
+                        }
+                    }
+                    div(class="about--content-connector bottom")
+                }
             }
         }
     }
--- src/components/SectionProfiles.rs
+++ src/components/SectionProfiles.rs
@@ -5,7 +5,25 @@ pub fn SectionProfiles<G: Html>() -> View<G> {
     view! {
         section(id="profiles"){
             div(class="header"){ "Profiles" }
-            div(class="profiles--container"){}
+            div(class="profiles--container"){
+                a(href="https://github.com/noxxxxxious", class="profiles--link") {
+                    img(src="images/github.png", alt="Github Logo")  
+                }
+                a(href="https://linkedin.com/in/seanmwfried", class="profiles--link") {
+                    img(src="images/linkedin.svg", alt="LinkedIn Logo")  
+                }
+                div(class="no-email") {
+                    div(class="no-email--header") { "Why no email?"}
+                    div(class="about--info--divider-diamond no-email--divider"){
+                        div(class="about--info--divider-diamond--line no-email--divider")
+                    }
+                    div(class="no-email--content") { "Let's be honest: If you email me with a job offer out of nowhere, I'm probably going to think that you are just trying to steal my information. Everyone has a LinkedIn now, and it offers much more peace of mind. If you would like to start a dialogue, please message me there." }
+                    div(class="no-email--thanks") { 
+                        span{ "Thank you for reading!" }
+                        span{ "-Sean" }
+                    }
+                }
+            }
         }
     }
 }
